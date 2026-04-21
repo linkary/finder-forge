862e0ba579af502de0c4522944aa67293d22045e
linkary <ilinkary@gmail.com>
2026-04-21 15:10:58 +0800

    Delay Finder rename until after service exit

--- helpers/finder_context.sh
+++ helpers/finder_context.sh
@@ -154,9 +154,59 @@ end run
 APPLESCRIPT
 }
 
+schedule_inline_rename_in_finder() {
+  if [[ "${FINDER_FORGE_DISABLE_INLINE_RENAME:-0}" == "1" ]]; then
+    return 1
+  fi
+
+  local target_path="${1}"
+
+  (
+    /bin/sleep 0.8
+    /usr/bin/osascript - "${target_path}" <<'APPLESCRIPT' >/dev/null
+on run argv
+  set targetFile to POSIX file (item 1 of argv) as alias
+
+  tell application "Finder"
+    activate
+    reveal targetFile
+    select targetFile
+  end tell
+
+  delay 0.2
+
+  try
+    tell application "System Events"
+      if not UI elements enabled then
+        return
+      end if
+
+      tell process "Finder"
+        set frontmost to true
+        key code 36
+      end tell
+
+      repeat 20 times
+        delay 0.05
+        tell process "Finder"
+          try
+            set focusedElement to value of attribute "AXFocusedUIElement"
+            if role description of focusedElement is "text field" then
+              exit repeat
+            end if
+          end try
+        end tell
+      end repeat
+    end tell
+  end try
+end run
+APPLESCRIPT
+  ) >/dev/null 2>&1 &
+}
+
 reveal_and_begin_rename_in_finder() {
   local target_path="${1}"
 
   reveal_in_finder "${target_path}"
-  begin_inline_rename_in_finder "${target_path}" || true
+  schedule_inline_rename_in_finder "${target_path}" || true
 }
